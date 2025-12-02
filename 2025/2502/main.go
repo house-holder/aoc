c31e4761394295f72fdf37b9ebf13109f4ad19e0
kh <kylemhouseholder@gmail.com>
2025-12-02 00:04:33 -0600

    2502 part 1 done

--- 2025/2502/main.go
+++ 2025/2502/main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
+
+func hasPattern(id int) bool {
+	idStr := strconv.Itoa(id)
+	length := len(idStr)
+	half := length / 2
+
+	for i := range half {
+		if idStr[i] != idStr[half+i] {
+			return false
+		}
+	}
+
+	return length == half*2
+}
+
+func evaluateRange(firstID int, lastID int, accumulator *int) {
+	for i := firstID; i <= lastID; i++ {
+		if hasPattern(i) {
+			fmt.Printf("   > Found ID %v has pattern\n", i)
+			*accumulator += i
+		}
+	}
+}
 
 func main() {
 	filename := "input.txt"
@@ -15,16 +40,32 @@ func main() {
 
 	file, err := os.Open(filename)
 	if err != nil {
-		log.Fatalf("Error opening file: %v", err)
+		log.Fatalf("Failed to open: %v", err)
 	}
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	lines := []string{}
-	for scanner.Scan() {
-		line := scanner.Text()
-		lines = append(lines, line)
+	body, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalf("Failed to read: %v", err)
 	}
 
-	fmt.Println(lines)
+	accumulator := 0
+
+	for ID := range strings.SplitSeq(string(body), ",") {
+		possRange := strings.Split(ID, "-")
+		fmt.Printf("Evaluating range: %v\n", possRange)
+
+		firstID, err := strconv.Atoi(possRange[0])
+		if err != nil {
+			log.Fatalf("Failed to convert: %v", err)
+		}
+
+		lastID, err := strconv.Atoi(possRange[1])
+		if err != nil {
+			log.Fatalf("Failed to convert: %v", err)
+		}
+
+		evaluateRange(firstID, lastID, &accumulator)
+		fmt.Printf("Accumulator: %v\n", accumulator)
+	}
 }
