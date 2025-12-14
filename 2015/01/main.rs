4f662bdd10948a62580201fc279da9c3777ca7e7
kh <kylemhouseholder@gmail.com>
2025-12-14 14:57:54 -0600

    gitignore updates for rust

--- 2015/01/main.rs
+++ 2015/01/main.rs
@@ -1,17 +1,9 @@
+#![allow(non_snake_case)]
+
 use std::env;
 use std::fs;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() < 2 {
-        eprintln!("Usage: {} <filename>", args[0]);
-        return;
-    }
-
-    let filename = &args[1];
-    let input = fs::read_to_string(filename).expect("Failed to read file");
-
+fn evalPart1(input: &String) -> i32 {
     let mut floor = 0;
     for ch in input.chars() {
         match ch {
@@ -20,8 +12,10 @@ fn main() {
             _ => {}
         }
     }
-    println!("Part 1: {}", floor);
+    return floor;
+}
 
+fn evalPart2(input: &String) -> i32 {
     let mut floor = 0;
     for (i, ch) in input.chars().enumerate() {
         match ch {
@@ -30,8 +24,23 @@ fn main() {
             _ => {}
         }
         if floor == -1 {
-            println!("Part 2: {}", i + 1);
-            break;
+            return (i + 1) as i32;
         }
     }
+    return -1;
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <filename>", args[0]);
+        return;
+    }
+    let filename = &args[1];
+    let input = fs::read_to_string(filename).expect("Failed to read file");
+
+    let result1 = evalPart1(&input);
+    println!("Part 1: {}", result1);
+    let result2 = evalPart2(&input);
+    println!("Part 2: {}", result2);
 }
